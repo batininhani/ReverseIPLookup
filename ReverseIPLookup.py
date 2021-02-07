f22dc91b073764278b5e1e764e3cd631089f7c1f
Batuhan Camcı <36328109+batininhani@users.noreply.github.com>
2021-02-07 23:44:10 +0300

    Update ReverseIPLookup.py

--- ReverseIPLookup.py
+++ ReverseIPLookup.py
@@ -3,12 +3,20 @@ from urllib.parse import urlencode, urlparse
 from urllib.request import urlopen, Request
 from urllib.error import HTTPError, URLError
 from bcolors import bcolors
-from bs4 import BeautifulSoup; import string, pyfiglet, socket, sys, re, time
+from bs4 import BeautifulSoup
+import string
+import pyfiglet
+import socket
+import sys
+import re
+import time
 
 hosts = []
 
-    # Print iterations progress
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+# Print iterations progress
+
+
+def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
     """
     Call in a loop to create terminal progress bar
     @params:
@@ -21,35 +29,41 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
         fill        - Optional  : bar fill character (Str)
         printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
     """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    percent = ("{0:." + str(decimals) + "f}").format(100 *
+                                                     (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=printEnd)
     # Print New Line on Complete
-    if iteration == total: 
+    if iteration == total:
         print()
 
+
 def displayMessage(msg):
-	s = ""
-	for i in msg:
-	    s += i                      
-	    print(s, end='')                                                    
-	    time.sleep(0.1)   
+    s = ""
+    for i in msg:
+        s += i
+        print(s, end='')
+        time.sleep(0.1)
+
 
 # Check https://regex101.com/r/A326u1/5 for reference
 DOMAIN_FORMAT = re.compile(
-    r"(?:^(\w{1,255}):(.{1,255})@|^)" # http basic authentication [optional]
-    r"(?:(?:(?=\S{0,253}(?:$|:))" # check full domain length to be less than or equal to 253 (starting after http basic auth, stopping before port)
-    r"((?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+" # check for at least one subdomain (maximum length per subdomain: 63 characters), dashes in between allowed
-    r"(?:[a-z0-9]{1,63})))" # check for top level domain, no dashes allowed
-    r"|localhost)" # accept also "localhost" only
-    r"(:\d{1,5})?", # port [optional]
+    r"(?:^(\w{1,255}):(.{1,255})@|^)"  # http basic authentication [optional]
+    # check full domain length to be less than or equal to 253 (starting after http basic auth, stopping before port)
+    r"(?:(?:(?=\S{0,253}(?:$|:))"
+    # check for at least one subdomain (maximum length per subdomain: 63 characters), dashes in between allowed
+    r"((?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+"
+    r"(?:[a-z0-9]{1,63})))"  # check for top level domain, no dashes allowed
+    r"|localhost)"  # accept also "localhost" only
+    r"(:\d{1,5})?",  # port [optional]
     re.IGNORECASE
 )
 SCHEME_FORMAT = re.compile(
-    r"^(http|hxxp|ftp|fxp)s?$", # scheme: http(s) or ftp(s)
+    r"^(http|hxxp|ftp|fxp)s?$",  # scheme: http(s) or ftp(s)
     re.IGNORECASE
 )
+
 
 def validate_url(url: str):
     url = url.strip()
@@ -58,7 +72,8 @@ def validate_url(url: str):
         raise Exception("No URL specified")
 
     if len(url) > 2048:
-        raise Exception("URL exceeds its maximum length of 2048 characters (given length={})".format(len(url)))
+        raise Exception(
+            "URL exceeds its maximum length of 2048 characters (given length={})".format(len(url)))
 
     result = urlparse(url)
     scheme = result.scheme
@@ -91,10 +106,14 @@ def search(remoteAddr):
     while(q):
 
         try:
-            address = (f"https://www.bing.com/search?q=ip:\"{remoteAddr}\"&first={first}1")
+            address = (
+                f"https://www.bing.com/search?q=ip:\"{remoteAddr}\"&first={first}1")
             custom_user_agent = "Mozilla/5.0 (Linux; Android 4.4; Nexus 7 Build/KRT16M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.92 Safari/537.36"
-            getRequest = Request(address, headers={"User-Agent": custom_user_agent}); page = urlopen(getRequest)
-            soup = BeautifulSoup(page.read(), "lxml"); links = soup.find_all("a", href=True)
+            getRequest = Request(
+                address, headers={"User-Agent": custom_user_agent})
+            page = urlopen(getRequest)
+            soup = BeautifulSoup(page.read(), "lxml")
+            links = soup.find_all("a", href=True)
             for link in links:
                 if(int(soup.select("#b_results > li.b_pag > div > nav > div.sb_center > ul > li > a.sb_pagS.sb_pagS_bp.sb_bp")[0].text) == (first + 1)):
                     pass
@@ -110,41 +129,45 @@ def search(remoteAddr):
                             hosts.append(domain)
                             print(bcolors.ok("[+]") + domain)
                             time.sleep(0.3)
-            
-            
+
                 else:
                     while(q == True):
-                        displayMessage("\r{}Found {} sites hosted on a given server\n".format(bcolors.okblue("[i]"), len(hosts)))
+                        displayMessage("\r{}Found {} sites hosted on a given server\n".format(
+                            bcolors.okblue("[i]"), len(hosts)))
                         q = False
 
-            first += 1  
+            first += 1
         except KeyboardInterrupt:
-            print("{}Found {} sites hosted on a given server".format(bcolors.okblue("[i]"), len(hosts)))
+            print("{}Found {} sites hosted on a given server".format(
+                bcolors.okblue("[i]"), len(hosts)))
         except IndexError:
-            continue    
+            continue
 
-print(pyfiglet.figlet_format("Reverse IP Lookup") + "\t\t\t\t\twritten by batininhani\n")
 
-try:  
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-    #print ("Socket successfully created") 
-except socket.error as err:  
+print(pyfiglet.figlet_format("Reverse IP Lookup") +
+      "\t\t\t\t\twritten by batininhani\n")
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #print ("Socket successfully created")
+except socket.error as err:
     #print ("socket creation failed with error %s" %(err))
     sys.exit(1)
-  
+
 remoteAddr = input("Remote Address: ")
 
-# default port for socket  
+# default port for socket
 port = 80
 
-try:  
+try:
     host_ip = socket.gethostbyname(remoteAddr)
     host = socket.gethostbyaddr(host_ip)
 
-except socket.gaierror:  
-  
-    # this means could not resolve the host  
-    displayMessage("\r%sThere was an error resolving the host" % bcolors.fail("[-]")) 
+except socket.gaierror:
+
+    # this means could not resolve the host
+    displayMessage("\r%sThere was an error resolving the host" %
+                   bcolors.fail("[-]"))
     sys.exit(1)
 
 except socket.herror:
@@ -152,19 +175,20 @@ except socket.herror:
     displayMessage("\r%sHost could not be found!" % (bcolors.fail("[-]")))
     sys.exit(1)
 
-# connecting to the server  
+# connecting to the server
 s.connect((host_ip, port))
-displayMessage("\rResolve Host: %s" % host[0]); print('\n')
+displayMessage("\rResolve Host: %s" % host[0])
+print('\n')
 s.close()
 search(host_ip)
 
 if(len(hosts) >= 1):
 
     file_name = remoteAddr + "_output.txt"
-    file_object  = open(file_name, "w+")
+    file_object = open(file_name, "w+")
     for i in hosts:
         file_object.write(i + '\n')
-    displayMessage("\r%sOutput has been successfully written to %s!" % (bcolors.okblue("[i]"), file_name)); print('\n')
+    displayMessage("\r%sOutput has been successfully written to %s!" %
+                   (bcolors.okblue("[i]"), file_name))
+    print('\n')
     file_object.close()
-    
-
